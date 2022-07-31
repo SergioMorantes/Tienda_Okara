ceba4fb6db792e6339c630e7138cee1b7911bf7c
Sergio Morantes <sergio.morantes434@gmail.com>
2022-07-30 20:12:53 -0500

    mi primer commit

---
+++ Config/Config.php
@@ -0,0 +1,38 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://localhost/tienda_virtual";
+
+	//Zona horaria
+	date_default_timezone_set('America/Bogota');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_tiendavirtual";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24.1989,00
+	const SPD = ",";
+	const SPM = ".";
+
+	//Simbolo de moneda
+	const SMONEY = "$";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "Tienda Virtual";
+	const EMAIL_REMITENTE = "no-reply@okara.com";
+	const NOMBRE_EMPESA = "Tienda Virtual";
+	const WEB_EMPRESA = "www.okara.com";
+
+	const CAT_SLIDER = "1,2,3";
+	const CAT_BANNER = "4,5,6";
+	
+
+
+
+	
+
+
+ ?>
---
+++ Models/ClientesModel.php
@@ -0,0 +1,142 @@
+<?php 
+class ClientesModel extends Mysql
+{
+	private $intIdUsuario;
+	private $strIdentificacion;
+	private $strNombre;
+	private $strApellido;
+	private $intTelefono;
+	private $strEmail;
+	private $strPassword;
+	private $strToken;
+	private $intTipoId;
+	private $intStatus;
+	private $strDireccion;
+	private $strBarrio;
+	private $strCiudad;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}	
+
+	public function insertCliente(string $identificacion, string $nombre, string $apellido, int $telefono, string $email, string $password, int $tipoid, string $direccion, string $barrio, string $ciudad){
+
+		$this->strIdentificacion = $identificacion;
+		$this->strNombre = $nombre;
+		$this->strApellido = $apellido;
+		$this->intTelefono = $telefono;
+		$this->strEmail = $email;
+		$this->strPassword = $password;
+		$this->intTipoId = $tipoid;
+		$this->strDireccion = $direccion;
+		$this->strBarrio = $barrio;
+		$this->strCiudad = $ciudad;
+
+		$return = 0;
+		$sql = "SELECT * FROM persona WHERE 
+				email_user = '{$this->strEmail}' or identificacion = '{$this->strIdentificacion}' ";
+		$request = $this->select_all($sql);
+
+		if(empty($request))
+		{
+			$query_insert  = "INSERT INTO persona(identificacion,nombres,apellidos,telefono,email_user,password,rolid,direccion,barrio,ciudad) 
+							  VALUES(?,?,?,?,?,?,?,?,?,?)";
+        	$arrData = array($this->strIdentificacion,
+    						$this->strNombre,
+    						$this->strApellido,
+    						$this->intTelefono,
+    						$this->strEmail,
+    						$this->strPassword,
+    						$this->intTipoId,
+    						$this->strDireccion,
+    						$this->strBarrio,
+    						$this->strCiudad);
+        	$request_insert = $this->insert($query_insert,$arrData);
+        	$return = $request_insert;
+		}else{
+			$return = "exist";
+		}
+        return $return;
+	}
+
+	public function selectClientes()
+	{
+		$sql = "SELECT idpersona,identificacion,nombres,apellidos,telefono,email_user,status 
+				FROM persona 
+				WHERE rolid = 7 and status != 0 ";
+		$request = $this->select_all($sql);
+		return $request;
+	}
+
+	public function selectCliente(int $idpersona){
+		$this->intIdUsuario = $idpersona;
+		$sql = "SELECT idpersona,identificacion,nombres,apellidos,telefono,email_user,direccion,barrio,ciudad,status, DATE_FORMAT(datecreated, '%d-%m-%Y') as fechaRegistro 
+				FROM persona
+				WHERE idpersona = $this->intIdUsuario and rolid = 7";
+		$request = $this->select($sql);
+		return $request;
+	}
+
+	public function updateCliente(int $idUsuario, string $identificacion, string $nombre, string $apellido, int $telefono, string $email, string $password, string $direccion, string $barrio, string $ciudad){
+
+		$this->intIdUsuario = $idUsuario;
+		$this->strIdentificacion = $identificacion;
+		$this->strNombre = $nombre;
+		$this->strApellido = $apellido;
+		$this->intTelefono = $telefono;
+		$this->strEmail = $email;
+		$this->strPassword = $password;
+		$this->strDireccion = $direccion;
+		$this->strBarrio = $barrio;
+		$this->strCiudad = $ciudad;
+
+		$sql = "SELECT * FROM persona WHERE (email_user = '{$this->strEmail}' AND idpersona != $this->intIdUsuario)
+									  OR (identificacion = '{$this->strIdentificacion}' AND idpersona != $this->intIdUsuario) ";
+		$request = $this->select_all($sql);
+
+		if(empty($request))
+		{
+			if($this->strPassword  != "")
+			{
+				$sql = "UPDATE persona SET identificacion=?, nombres=?, apellidos=?, telefono=?, email_user=?, password=?, direccion=?, barrio=?, ciudad=? 
+						WHERE idpersona = $this->intIdUsuario ";
+				$arrData = array($this->strIdentificacion,
+        						$this->strNombre,
+        						$this->strApellido,
+        						$this->intTelefono,
+        						$this->strEmail,
+        						$this->strPassword,
+        						$this->strDireccion,
+        						$this->strBarrio,
+        						$this->strCiudad);
+			}else{
+				$sql = "UPDATE persona SET identificacion=?, nombres=?, apellidos=?, telefono=?, email_user=?, direccion=?, barrio=?, ciudad=?  
+						WHERE idpersona = $this->intIdUsuario ";
+				$arrData = array($this->strIdentificacion,
+        						$this->strNombre,
+        						$this->strApellido,
+        						$this->intTelefono,
+        						$this->strEmail,
+        						$this->strDireccion,
+        						$this->strBarrio,
+        						$this->strCiudad);
+			}
+			$request = $this->update($sql,$arrData);
+		}else{
+			$request = "exist";
+		}
+		return $request;
+	}
+
+	public function deleteCliente(int $intIdpersona)
+	{
+		$this->intIdUsuario = $intIdpersona;
+		$sql = "UPDATE persona SET status = ? WHERE idpersona = $this->intIdUsuario ";
+		$arrData = array(0);
+		$request = $this->update($sql,$arrData);
+		return $request;
+	}
+}
+
+ ?>
