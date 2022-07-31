8341da527e0e55b36190f5b1594922857dd3e008
Sergio Morantes <sergio.morantes434@gmail.com>
2022-07-30 20:22:06 -0500

    modificacion de bienvenida en el header

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -45,7 +45,7 @@
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Bienvenido usuario: Rocio Gordillo
+						Bienvenido a la Tienda Virtual de Okara
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
